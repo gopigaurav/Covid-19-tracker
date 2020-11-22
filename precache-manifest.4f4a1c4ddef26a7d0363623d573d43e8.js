38d014a71efc5021bafc6b86f645cfdf60bebcd0
unknown <gopigaurav9@gmail.com>
2020-11-22 22:29:33 +0530

    Updates

---
+++ precache-manifest.4f4a1c4ddef26a7d0363623d573d43e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4152873abb3302488090c7b6259b4dc8",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "9dd651efde4d822e04ed",
+    "url": "/Covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "46b26ec11ccdcbeadd0d",
+    "url": "/Covid-19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "9dd651efde4d822e04ed",
+    "url": "/Covid-19-tracker/static/js/2.0ca6990e.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-tracker/static/js/2.0ca6990e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b26ec11ccdcbeadd0d",
+    "url": "/Covid-19-tracker/static/js/main.5768a4af.chunk.js"
+  },
+  {
+    "revision": "93c322cfa2ae1d702392",
+    "url": "/Covid-19-tracker/static/js/runtime-main.008c7622.js"
+  }
+]);
